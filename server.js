add8f4925104e76a67336db90dc357571bfef1b3
rakeshmuduli128-cpu <rakeshmuduli128@gmail.com>
2025-09-06 13:47:40 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Connect MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+// Sample Route
+app.get('/', (req, res) => {
+  res.send('SynergySphere backend is running.');
+});
+
+const userRoutes = require('./routes/users');
+app.use('/api/users', userRoutes);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
